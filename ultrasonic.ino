6eed2770172cc49e4d827b3cf45c6e82a9103e38
akshatha39 <42582360+akshatha39@users.noreply.github.com>
2018-08-21 22:09:52 +0530

    My first program

---
+++ ultrasonic.ino
@@ -0,0 +1,65 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+int led11=5;
+int led22=6;
+void led1();
+void led2();
+// defines variables
+int duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT);                    // Sets the trigPin as an Output
+pinMode(echoPin, INPUT);                       // Sets the echoPin as an Input
+Serial.begin(9600);                       // Starts the serial communication
+}
+void loop() {
+                                   // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+                            // Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(200);
+if(distance<10){
+led1();
+Serial.println("n"); 
+}
+else if(10< distance <20){
+  led2();
+  Serial.println("s");
+}
+else{
+  digitalWrite(led11,LOW);
+  delay;
+  digitalWrite(led22,LOW);
+  delay;
+  
+}
+}
+void led1()
+{
+  Serial.println("led1 on");
+  digitalWrite(led11,HIGH);
+  delay(200);
+   digitalWrite(led11,LOW);
+   delay(200);
+}
+void led2()
+{
+Serial.println("led2 on");
+  digitalWrite(led22,HIGH);
+  delay(200);
+   digitalWrite(led22,LOW);
+   delay(200);
+}
+
+
